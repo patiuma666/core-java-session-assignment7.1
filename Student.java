36de923372958043df27eb74ddcfb838590684bf
patiuma666 <patiuma666@gmail.com>
2017-07-09 21:44:59 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,29 @@
+package package1;
+import java.io.IOException;
+import java.util.*;
+ public class Student {
+ private String name =" uma";
+ private int rollno = 10;
+ public void accept ()throws IOException{
+	 Scanner sc =new Scanner(System.in);
+	 System.out.println("enter name: "+name );
+	 
+	 name=sc.toString();
+	 System.out.println("rollno: "+rollno );
+	 rollno=sc.nextInt();
+ }
+ }
+ 
+/* class Demo{
+	 
+	 public static void main(String []args) throws IOException{
+		 
+		 Student s =new Student();
+		 s.accept();
+		 
+	 }*/
+	 
+	 
+ 
+ 
+ 
